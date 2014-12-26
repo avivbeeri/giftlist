1b73c00042de59eceb8d9f51e411af1b71844f8a
Aviv Beeri <avbeer@gmail.com>
2014-12-26 15:48:04 +0000

    Things mostly work

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var url = require('url');
 var router = require("restroute");
 var port = process.env.PORT || 8080;
 var redis = require("redis");
+var mime = require('mime');
 var client;
 
 if (process.env.REDISCLOUD_URL) {
@@ -18,14 +19,30 @@ client.on("error", function (err) {
     console.log("Error " + err);
 });
 
-router.get("/", function (req,res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  var file = fs.createReadStream("./index.html");
+router.get("/:name", serveStatic);
+router.get("/", serveStatic);
+        
+function serveStatic(req,res) {
+  var nameParam = req.params.name;
+  var name;
+  if (nameParam && nameParam !== "favicon.ico") {
+      name = require("url").parse(nameParam, true).pathname;
+  } else if (nameParam !== "favicon.ico") {
+      name = "index.html";
+  } else {
+      res.writeHead(204);
+      res.end();
+      return;
+  }
+
+  var type = mime.lookup('./' + name);
+  res.writeHead(200, { 'Content-Type' : type });
+  var file = fs.createReadStream("./" + name);
   file.pipe(res);
-});
+};
 
 router.get("/api/gifts", function (req,res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.writeHead(200, {'Content-Type': 'application/json'});
     client.keys("gift:*", function (err, replies) {
         var query = client.multi();
         replies.forEach(function (giftName) {
@@ -39,12 +56,51 @@ router.get("/api/gifts", function (req,res) {
     });
 });
 
+
+router.post("/api/gifts/new", function (req, res) {
+    var body = "";
+    req.on('data', function (chunk) {
+        body += chunk;
+    });
+    req.on('end', function () {
+        var qs = require("querystring");
+        var data = qs.parse(body);
+        getNewUUID(function (e, uuid) {
+            client.hmset("gift:" + uuid, 
+                         "id", uuid,
+                         "name", data.name,
+                         "quantity", data.quantity,
+                         "description", data.description,
+                         "image", data.image,
+                         "link", data.link);
+        });
+        res.writeHead(201);
+        res.end();
+    });
+});
+
+router.get("/api/gifts/:id/delete", function (req, res) {
+    res.writeHead(200, {'Context-Type' : "application/json"});
+    client.hincrby("gift:" + req.params.id, "quantity", "-1", function (err, reply) {
+        if (reply <= 0) {
+            client.del("gift:" + req.params.id, function (err, reply) {
+                res.end();   
+            });
+        } else {
+            res.end(JSON.stringify(reply));   
+        }
+    });
+});
+
+
 router.onError(function (req,res) {
-  console.log("Error!");
+  console.log("Error!" + "Stuff?");
   res.writeHead(404);
   res.end("Error happened");
 });
 
 http.createServer(router.go).listen(port);
 
-
+function getNewUUID(callback) {
+    client.incr("UUID", callback);
+}
